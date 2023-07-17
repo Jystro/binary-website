3b71caab983d2a8d9b064d3246a5e82f8287f3d5
Jystro <35638179+Jystro@users.noreply.github.com>
2023-07-17 17:52:27 +0200

    Added 404 error page, test for 404 page, route for 404 page. Modified .drone.yml steps name, updated package.json

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.send("Home");
+});
+
 app.get("/hello-world", (req, res) => {
 	res.send("Hello World!");
+});
+
+
+app.use((req, res) => {
+	res.status(404).sendFile(__dirname + "/public/404.html");
 });
 
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -2,12 +2,22 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("Test the hello-world path", () => {
-	test("It should response 'Hello World!'", () => {
+	test("It should respond 'Hello World!'", () => {
 		return request(app)
 			.get("/hello-world")
 			.then(resp => {
 				expect(resp.statusCode).toBe(200);
 				expect(resp.text).toBe("Hello World!");
+			});
+		});
+});
+
+describe("Test the 404 page", () => {
+	test("It should respond with the 404 error code", () => {
+		return request(app)
+			.get("/page-that-does-not-exist")
+			.then(resp => {
+				expect(resp.statusCode).toBe(404);
 			});
 		});
 });
